d1efd3b6d1ef083e25ffc57c6bc55bc4b5073b55
andrewtran274 <andrewtran274@gmail.com>
2023-10-30 23:24:01 +0700

    form commit

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,5 @@
-import { Link, Outlet, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contacts";
+import { Form, Link, Outlet, useLoaderData } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
 
 export async function loader() {
   const contacts = await getContacts();
@@ -24,9 +24,9 @@ export default function Root() {
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (

5ecc15262d28a8a54b231e73f1e6afaa3d7f8621
andrewtran274 <andrewtran274@gmail.com>
2023-10-31 20:21:38 +0700

    init commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -15,10 +15,12 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,6 +5,10 @@ export async function loader() {
   const contacts = await getContacts();
   return { contacts };
 }
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 export default function Root() {
   const { contacts } = useLoaderData();
